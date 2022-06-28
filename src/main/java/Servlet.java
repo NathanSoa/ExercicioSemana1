fa06321c2d412dd5dda513bdbe184c0a4e4d1e38
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-06-28 17:01:40 -0300

    add methods to calculate

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Servlet")
+@WebServlet("/converter")
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -17,6 +17,23 @@ public class Servlet extends HttpServlet {
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String tipo = request.getParameter("conversao");
+		String valor = request.getParameter("valor");
+		double resultado = 0;
+		
+		if(tipo.equalsIgnoreCase("celsius"))
+			resultado = CelsiusToFahrenheit(valor);
+		else
+			resultado = FahrenheitToCelsius(valor);
+		
+		System.out.println(resultado);
 	}
-
+	
+	private double CelsiusToFahrenheit(String valor) {
+		return ((Double.valueOf(valor) * 9) / 5) + 32;
+	}
+	
+	private double FahrenheitToCelsius(String valor) {	
+		return ((Double.valueOf(valor) - 32) * 5) / 9;
+	}
 }
